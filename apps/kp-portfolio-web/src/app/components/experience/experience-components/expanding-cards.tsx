d026271b2dbbfbb805b6a5b42d8f43432f83002b
Vy Nguyen <vyx.nguyen96@gmail.com>
2025-04-01 14:04:29 -0400

    20250401 - 0140A: Refractoring and move hooks to library - Home page: 90% - Summary page: 100% - Experience page: 50% - Skills page: 5% - Projects page: 1% - KarPort v1: 0% - KarPort v2: 0% - This page: 5%

--- apps/kp-portfolio-web/src/app/components/experience/experience-components/expanding-cards.tsx
+++ apps/kp-portfolio-web/src/app/components/experience/experience-components/expanding-cards.tsx
@@ -4,7 +4,7 @@ import ExpandingCardDetail from '@/app/components/experience/experience-componen
 import { useTranslation } from 'react-i18next';
 import RevealManual from '@/app/components/shared/motion/RevealManual';
 import { EXPERIENCE_CARDS } from '@/app/components/shared/Constant';
-import useCardMouseHover from '@/hooks/useCardMouseHover';
+import { useMouseHover } from '@kp-react-lib/kp-react-common';
 
 function revealSection(sectionIndex: number){
   console.log('Reveal this section ' + sectionIndex)
@@ -13,7 +13,7 @@ function revealSection(sectionIndex: number){
 export function ExpandingCards({revealFunc}:{revealFunc: () => void}) {
   const {t} = useTranslation()
   const toggleRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const activeIndex = useCardMouseHover(toggleRefs)
+  const activeIndex = useMouseHover(toggleRefs)
   document.documentElement.classList.add('dark');
 
   return (
@@ -22,7 +22,7 @@ export function ExpandingCards({revealFunc}:{revealFunc: () => void}) {
         <div className={styles['expanding-cards-wrapper']}>
           <div className={styles['expanding-cards-container']}>
             {EXPERIENCE_CARDS.map((exp, index) => (
-              <div ref={(el) => { toggleRefs.current[index] = el; }} className={styles['expanding-card'] + ` toggle ${activeIndex === index ? styles.active : ''}`}>
+              <div key={exp.title} ref={(el) => { toggleRefs.current[index] = el; }} className={styles['expanding-card'] + ` toggle ${activeIndex === index ? styles.active : ''}`}>
                 <img src={exp.img} alt={exp.title}/>
                 <RevealManual duration={0.5} delay={1} className={`opacity-0 transition-opacity duration-200 ${activeIndex === index ? 'z-5 opacity-100 duration-500 delay-200':''} relative h-full w-full`}>
                   <ExpandingCardDetail reveal={()=> revealSection(index)} fullsize={activeIndex === index} title={t(exp.title)} location={t(exp.location)} period={t(exp.period)}/>
