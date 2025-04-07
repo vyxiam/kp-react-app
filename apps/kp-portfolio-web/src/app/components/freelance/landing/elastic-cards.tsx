7d6c5713935bb32ae8de5d45516ba9eeb7635e02
Vy Nguyen <vyx.nguyen96@gmail.com>
2025-04-07 12:16:23 -0400

    20250407 - 1220P: Draft Freelance Elastic Card v2 - Home page: 90% - Summary page: 100% - Experience page: 95% (animation back to cards - required) - Skills page: 100% - Projects page: 5% - KarPort v1: 0% - KarPort v2: 0% - This page: 5%

--- apps/kp-portfolio-web/src/app/components/freelance/landing/elastic-cards.tsx
+++ apps/kp-portfolio-web/src/app/components/freelance/landing/elastic-cards.tsx
@@ -3,7 +3,22 @@ import { gsap } from 'gsap';
 import styles from './elastic.module.css'
 import { useEffect, useRef, useState } from 'react';
 
-export function ElasticCards({content}:{content: string[]}) {
+function getItemWidth(totalCards: number): [string, string, string, string]{
+  const perItem = 75/totalCards
+  const min = Math.ceil(perItem/2)
+  const max = 75 - min*(totalCards - 1)
+  return [`${perItem}vw`, `${max}vw`, `${min}vw`, `${min + 2}vw`]
+}
+
+interface ElasticProps{
+  img: string;
+  location: string;
+  title: string;
+  period: string;
+}
+
+export function ElasticCards({content, test}:{content: ElasticProps[], test: string[]}) {
+  const [itemWidth, maxItemWidth, minItemWidth, minPlusItemWidth] = getItemWidth(content.length)
   const [expandedIndex, setExpandedIndex] = useState(-100);
   const toggleRefs = useRef<(HTMLDivElement | null)[]>([]);
   const previousState = useRef({ index: -1, wasExpanded: false });
@@ -26,7 +41,7 @@ export function ElasticCards({content}:{content: string[]}) {
       if ((index === expandedIndex)){
         // Animate clicked item to EXPAND
         animation = gsap.to(item, {
-          width: '42vw',
+          width: maxItemWidth,
           duration: 2.5,
           ease: 'elastic(1, .3)'
         });
@@ -34,9 +49,9 @@ export function ElasticCards({content}:{content: string[]}) {
       else if (index === previousState.current.index){
         // Animate clicked item to COLLAPSE
         animation = gsap.fromTo(item,
-          { width: "42vw" },
+          { width: maxItemWidth},
           {
-            width: "15vw",
+            width: itemWidth,
             duration: 2.5,
             ease: "elastic(1, 0.3)"
           }
@@ -45,7 +60,7 @@ export function ElasticCards({content}:{content: string[]}) {
       else if (!wasExpanded && previousState.current.index < 0){
         // Animate other items when an item is EXPANDING and in initial state
         animation = gsap.to(item, {
-          width: '8vw',
+          width: minItemWidth,
           duration: 2,
           ease: 'elastic(1, .6)'
         });
@@ -53,9 +68,9 @@ export function ElasticCards({content}:{content: string[]}) {
       else if (!wasExpanded && previousState.current.index >= 0){
         // Animate other items when the clicked item is EXPANDING and NOT in initial state
         animation = gsap.fromTo(item,
-          {width: '10vw'},
+          {width: minPlusItemWidth},
           {
-            width: '8vw',
+            width: minItemWidth,
             duration: 2,
             ease: 'elastic(3, .3)'
           });
@@ -63,9 +78,9 @@ export function ElasticCards({content}:{content: string[]}) {
       else if (wasExpanded){
         // Animate other items when the clicked item is COLLAPSED
         animation = gsap.fromTo(item,
-          { width: '8vw' },
+          { width: minItemWidth},
           {
-            width: '15vw',
+            width: itemWidth,
             duration: 2,
             ease: 'elastic(1, .6)'
           })
@@ -90,11 +105,11 @@ export function ElasticCards({content}:{content: string[]}) {
     <section className={styles.group}>
       {content.map((item, index) => (
         <div
-          key={uuid()}
-          ref={(el) => {toggleRefs.current[index] = el}}
-          className={`${styles.item} ${item}`}
-          onClick={() => handleClick(index)}
-        />
+          key={uuid()} ref={(el) => {toggleRefs.current[index] = el}}
+          className={`${styles.item} ${test[index]} w-[${itemWidth}]`} onClick={() => handleClick(index)}
+        >
+          <img src={item.img} alt={item.title} className={`object-cover w-full h-full ${styles.cover}`}/>
+        </div>
       ))}
     </section>
   );
