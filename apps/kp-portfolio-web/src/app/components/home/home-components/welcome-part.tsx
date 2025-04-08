b68f8814b36541465662fd47661fe62579594669
Vy Nguyen <vyx.nguyen96@gmail.com>
2025-04-08 01:48:09 -0400

    20250408 - 0150A: Complete welcome part - Home page: 95% - Summary page: 100% - Experience page: 95% (animation back to cards - required) - Skills page: 100% - Projects page: 85% - KarPort v1: 0% - KarPort v2: 0% - This page: 5%

--- apps/kp-portfolio-web/src/app/components/home/home-components/welcome-part.tsx
+++ apps/kp-portfolio-web/src/app/components/home/home-components/welcome-part.tsx
@@ -1,47 +1,75 @@
 import { ArrowRight } from 'lucide-react';
 import { HOME_HIGHLIGHTS } from '@/app/components/shared/Constant';
 import { useTranslation } from 'react-i18next';
-import { DotPrimaryButton, KPOutlineButton, uuid } from '@kp-react-lib/kp-react-common';
+import {
+  DotPrimaryButton,
+  GlitchTitle,
+  KPOutlineButton,
+  Reveal,
+  useTimeout,
+  uuid,
+  GlowingCard,
+  StaggeredFlipInTitle,
+} from '@kp-react-lib/kp-react-common';
 import { useNavigate } from 'react-router';
+import VACODE from '@/assets/vacode.jpeg';
 
 export function HomeWelcome() {
   const {t} = useTranslation()
   const navigate = useNavigate()
+  const showFirstStatus = useTimeout(1.2)
+  const showSecondStatus = useTimeout(1.5)
   return (
-    <section className="">
-      {/*<div className="container flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center space-y-8 py-24 text-center md:py-32">*/}
-      <div className="2xl:container flex flex-col items-center justify-center space-y-8 py-16 text-center md:py-24">
-        <div className="space-y-4">
-          <h1 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
-            {t('home.welcome.to')}
-            <br />
-            {t('home.my.site')}
-          </h1>
-          <p className="mx-auto max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            <span dangerouslySetInnerHTML={{__html: t('home.welcome.description')}}></span>
-          </p>
+    <section className='flex flex-col 2xl:flex-row'>
+      <div className='flex flex-col 2xl:w-[70%]'>
+        {/*<div className="container flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center space-y-8 py-24 text-center md:py-32">*/}
+        <div className="2xl:container flex flex-col items-center justify-center space-y-8 py-16 text-center md:py-24">
+          <div className="space-y-4">
+            <h1 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
+              {t('home.welcome.to')}
+              <br />
+              {t('home.my.site')}
+            </h1>
+            <p className="mx-auto max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+              <span dangerouslySetInnerHTML={{__html: t('home.welcome.description')}}></span>
+            </p>
+          </div>
+          <div className="flex gap-4 flex-col sm:flex-row">
+            <DotPrimaryButton onClick={() => navigate('/summary')} text={t('home.primary.button')}><ArrowRight className="ml-2 h-4 w-4" /></DotPrimaryButton>
+            <KPOutlineButton onClick={() => navigate('/freelance-projects')} text={t('home.secondary.button')}></KPOutlineButton>
+          </div>
         </div>
-        <div className="flex gap-4 flex-col sm:flex-row">
-          <DotPrimaryButton onClick={() => navigate('/summary')} text={t('home.primary.button')}><ArrowRight className="ml-2 h-4 w-4" /></DotPrimaryButton>
-          <KPOutlineButton onClick={() => navigate('/freelance-projects')} text={t('home.secondary.button')}></KPOutlineButton>
+
+        <div className="2xl:container space-y-16 py-2 md:py-24">
+          <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 md:grid-cols-2">
+            {HOME_HIGHLIGHTS.map((highlight) => (
+              <div key={uuid()} className="relative overflow-hidden rounded-lg border bg-background p-8">
+                <div className="flex items-center gap-4">
+                  <highlight.icon className="h-8 w-8" />
+                  <h3 className="font-bold">{t(highlight.title)}</h3>
+                </div>
+                <p className="mt-2 text-muted-foreground">{t(highlight.description)}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className="2xl:container space-y-16 py-2 md:py-24">
-        <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 md:grid-cols-2">
-          {HOME_HIGHLIGHTS.map((highlight) => (
-            <div key={uuid()} className="relative overflow-hidden rounded-lg border bg-background p-8">
-              <div className="flex items-center gap-4">
-                <highlight.icon className="h-8 w-8" />
-                <h3 className="font-bold">{t(highlight.title)}</h3>
-              </div>
-              <p className="mt-2 text-muted-foreground">{t(highlight.description)}</p>
-            </div>
-          ))}
+      <div className='flex flex-col items-center'>
+        <Reveal><GlowingCard title={'Full stack'} img={VACODE} className={'mt-20 2xl:mt-40'}/></Reveal>
+        <StaggeredFlipInTitle text={'Full-stack'} className={'mt-12 text-3xl'}/>
+        <div className='flex flex-col items-center gap-3 mt-4 text-4xl'>
+          {showFirstStatus &&
+            (<GlitchTitle
+              tag={'h3'} randomColor={'text-purple-500'}
+              className={'text-blue-200'} title={'Software Developer'} />)}
+          {showSecondStatus &&
+            (<GlitchTitle
+              tag={'h3'} randomColor={'text-purple-500'}
+              className={'text-blue-200'} title={'Web Developer'} />)}
         </div>
       </div>
     </section>
-
   );
 }
 
